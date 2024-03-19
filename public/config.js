d6dcbeef48e0efa22bdae7c90d8ce5a39ca24dc5
Villager A <100194237+habataku@users.noreply.github.com>
2024-03-19 10:28:17 +0900

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,10 +1,10 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://simple-proxy.sxdce.workers.dev",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMTdhYmEzMmJmNGVjZWFmYmQ1OTAzOGM5NTJlMDk5NCIsInN1YiI6IjY1ZjhlODdjNTQ5ZGRhMDEyZTdjMmM5YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VYpTOUJANWIqafLyPMYAy4sRD-xxiqim7GbkIhjY7VI",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
